46d408644a864e6fedf18e9203d576360a80d9ab
bruno <braphaelperez98@gmail.com>
2024-11-01 15:23:26 -0300

    Segundo commit

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def exibir_nome():
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurante')
-    print('3. Ativar restaurante')
+    print('3. Ativa ou Desativar Restaurante')
     print('4. Sair\n ')
 
 def finalizar_app():
@@ -19,7 +19,10 @@ def finalizar_app():
    
 def exibir_subtitulo(texto):
     os.system('cls')
+    linha = '*' * len(texto)
+    print(linha)
     print(texto)
+    print (linha)
     print()
     
 def retorno_menu():
@@ -42,14 +45,36 @@ def cadastrar_novo_restaurante():
 def listar_restaurantes():
     exibir_subtitulo('Lista de Restaurantes')
     
+    print(f'{'Nome do restaurente'.ljust(20)} | {'Categoria'.ljust(20)} | Status\n')
     for restaurante in restaurantes:
         nome_restaurante = restaurante['nome']
         categoria = restaurante['categoria']
-        ativo = restaurante['ativo']
-        print(f'*{nome_restaurante} | {categoria} |{ativo}\n')
+        ativo = 'ativado' if restaurante['ativo'] else 'desativado'
+               
+        print(f'{nome_restaurante.ljust(20)} | {categoria.ljust(20)} |{ativo}\n')
 
     retorno_menu()
 
+def alternar_estado_restaurante():
+    exibir_subtitulo('ALTERNANDO O ESTADO DO RESTAURANTE')
+    nome_restaurante = input('Digite o nome do restaurante que deseja alterar o estado: ')
+    restaurante_encontrado = False
+    
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']   #basicamente é um swap de estado como se encontra na lista
+            mensagem = (f'O restaurante {nome_restaurante} foi ativado com sucesso' 
+                         if restaurante['ativo'] #se ele der swap para verdadeiro 
+                         else f'O restaurante {nome_restaurante} foi desativado com sucesso') #se der swap para falso
+            print(mensagem)
+            break  # Saia do loop após encontrar o restaurante
+
+    if not restaurante_encontrado:
+        print('O restaurante não foi encontrado')
+    
+    retorno_menu()
+    
 def escolher_opcoes():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
@@ -59,7 +84,7 @@ def escolher_opcoes():
         elif opcao_escolhida == 2:
             listar_restaurantes()
         elif opcao_escolhida == 3:
-            print('Ativar restaurante')
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4:
                 finalizar_app()
         else:
